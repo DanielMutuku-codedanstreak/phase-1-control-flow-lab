4abf8dbc5cbc6ee72a7a8e2409791d641feac917
Daniel Mutuku <danielmakau507@gmail.com>
2023-09-18 12:37:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let distance;
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400 && distance >= 0){
+    return('This one is on me!')
+  }else if (distance > 2000 && distance < 2500){
+    return('I will gladly take your thirty bucks.')
+  }else{
+    return('No can do.')
+  }
 }
-
-function switchOnCharmFromTip(){
+let city;
+function ternaryCheckCity(city){
   // Write your code here!
+  const response = city === 'NYC' ? 'Ok, sounds good.' : 'No go.' 
+  return response
+}
+let tip;
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
